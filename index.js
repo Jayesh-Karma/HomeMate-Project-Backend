113b78a57da855c7ac1f64471102802eaee506d0
Jayesh Karma <126589168+Jayesh-Karma@users.noreply.github.com>
2025-03-23 20:14:17 +0530

    Updated CORS Policy in index

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ dotenv.config();
 
 // middlewares -->
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:5176'],  // Only allow this domain to make requests
+    origin: ['http://localhost:5173', 'https://home-mate-project-frontend.vercel.app'],  // Only allow this domain to make requests
     methods: ['GET', 'POST'], // Allowed HTTP methods
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials:true  // Allowed headers
