3bd3cff1fb3ec0c60f393113785ce540bcde730d
Jayesh Karma <126589168+Jayesh-Karma@users.noreply.github.com>
2025-03-23 19:50:41 +0530

    Changed CORS

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ dotenv.config();
 
 // middlewares -->
 app.use(cors({
-    origin: 'http://localhost:5173',  // Only allow this domain to make requests
+    origin: ['http://localhost:5173', 'http://localhost:5176'],  // Only allow this domain to make requests
     methods: ['GET', 'POST'], // Allowed HTTP methods
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials:true  // Allowed headers
